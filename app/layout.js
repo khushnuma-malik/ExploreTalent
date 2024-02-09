5c38a4f70c2defd173d38390c7aad967d4ab364a
khushnuma-malik <149143119+khushnuma-malik@users.noreply.github.com>
2024-02-08 23:53:55 -0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
       <div className='whatsapp'>
       <a href=' https://wa.me/919027436118 '> <img src='https://pngimg.com/d/whatsapp_PNG1.png '/></a></div>
       <div className='scroll'>
-      <Link href='/'> <img src='https://ps.w.org/scrollup-master/assets/icon-256x256.png?rev=1058878  '/></Link></div>
+      <Link href={'/'}> <img src='https://ps.w.org/scrollup-master/assets/icon-256x256.png?rev=1058878  '/></Link></div>
     
         {children}
         <Footer/>
